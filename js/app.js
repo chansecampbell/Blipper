3ac094a182a7f5bc4f569467cf1aff0c47d8948e
chansecampbell <chansecampbell@gmail.com>
2016-03-24 23:00:57 +0000

    Updated the game but I think I forgot to commit earlier. Ooops.

--- js/app.js
+++ js/app.js
@@ -7,10 +7,43 @@
 ////// Your goal is to strategically make your way across the screen bottom to top and escape the horror house.
 ///// You either have to be strategic with your moves to survive but you are also rewarded for the less moves you can complete the level within.
 
+//// Dom content is loaded
 $(function(){
 
+var player = $('.player');
+console.log(player)
+/// Click event is set up to listen on all id's by their class type.
 $('li').on("click", function(){
-  console.log(this.id);
+  // console.log(this.id);
+  $('player').removeAttr('class');
+  /// Remove all instances of this class after the click so that it doesnt trail like it currently does.
+ var player = $(this).addClass('player');
+
+
+
+/// Need to plan what happens as a consequence of the click? 
+// These are the potential outcomes:
+// If the square is empty and next to the player then move to it.
+// If the square has an enemy and is next to the to the player then move to it and lose a life.
+// If the square has an item and is next to the player then move to it and gain a life/powerup.
+// If the square is the exit then the level is complete and a new level is generated.
+// If the square is not next to the player then move invalid.
 })
+
+/// Variables needed
+
+var score = 0;
+var lives = 3;
+var player;
+// console.log(player)
+var zombie1;
+var zombie2;
+var zombie3;
+var powerUp1;
+var powerUp2;
+
+
+
+
 
 })
