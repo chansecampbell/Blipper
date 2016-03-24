5cc26d30aeb0fc6fce84ca3413ca309cca55417e
chansecampbell <chansecampbell@gmail.com>
2016-03-24 17:47:01 +0000

    Started the bases for my survival game. Right now I have a 6x6 grid of li's that are all identifiable when clicked by their id.

---
+++ js/app.js
@@ -0,0 +1,16 @@
+////// Survival Game
+////// 6 x 6 grid 
+////// Player starts at the bottom of the grid
+////// You can use the directional keys to move across the grid.
+////// There are things in your way that damage your HP - 3 lives and your dead.
+////// There are also things on the screen that give you HP back - 3 lives max.
+////// Your goal is to strategically make your way across the screen bottom to top and escape the horror house.
+///// You either have to be strategic with your moves to survive but you are also rewarded for the less moves you can complete the level within.
+
+$(function(){
+
+$('li').on("click", function(){
+  console.log(this.id);
+})
+
+})
