debe219d1a7bcc29f2383f2c2aeece1dd23aa56e
chansecampbell <chansecampbell@gmail.com>
2016-03-25 06:44:18 +0000

    Got to the stage where I'm swapping classes along a row, meaning values can be swapped and events could happen, but haven't figured out how to do that upwards yet.

--- js/app.js
+++ js/app.js
@@ -10,16 +10,34 @@
 //// Dom content is loaded
 $(function(){
 
-var player = $('.player');
-console.log(player)
-/// Click event is set up to listen on all id's by their class type.
+  // $.fn.toggle2classes = function(class1, class2){
+  //   if( !class1 || !class2 )
+  //     return this;
+
+  //   return this.each(function(){
+  //     var $elm = $(this);
+
+  //     if( $elm.hasClass(class1) || $elm.hasClass(class2) )
+  //       $elm.toggleClass(class1 +' '+ class2);
+
+  //     else
+  //       $elm.addClass(class1);
+  //   });
+  // };
+
+/// Click event is set up to listen on all li's on the page.
 $('li').on("click", function(){
-  // console.log(this.id);
-  $('player').removeAttr('class');
-  /// Remove all instances of this class after the click so that it doesnt trail like it currently does.
- var player = $(this).addClass('player');
+  var clickedId= $(this).attr("class");
+  // var hello = $('li').class();
+  // (console.log(hello));
+  // Whichever li is clicked, add the class 'player' to it and remove it from the currently existing one
+ $(this).addClass('player').removeClass('floor');
+ $(this).next('li').removeClass('player');
+ // $('li').toggle2classes('player', clickedId);
+})
 
 
+ })
 
 /// Need to plan what happens as a consequence of the click? 
 // These are the potential outcomes:
@@ -28,22 +46,46 @@ $('li').on("click", function(){
 // If the square has an item and is next to the player then move to it and gain a life/powerup.
 // If the square is the exit then the level is complete and a new level is generated.
 // If the square is not next to the player then move invalid.
-})
 
 /// Variables needed
 
-var score = 0;
-var lives = 3;
-var player;
+// var score = 0;
+// var lives = 3;
+// var player;
+// // console.log(player)
+// var zombie1;
+// var zombie2;
+// var zombie3;
+// var powerUp1;
+// var powerUp2;
+
+
+
+
+
+// var exit = $('.exit');
 // console.log(player)
-var zombie1;
-var zombie2;
-var zombie3;
-var powerUp1;
-var powerUp2;
+
+// $('.menu-toggle').click( function() {
+    // var i = 0;
+// } );
+ // $('player').removeAttr('class');
+ /// Remove all instances of this class after the click so that it doesnt trail like it currently does.
+// var player =
+// var player = $('.player');
+// var li = document.getElementsByClassName('li');
+
+// for (var i = 0; i < li.length; i++){
+//  li[i].addEventListener("click", function(){
+//    console.log(this);
+//  })
+// $('.player').removeClass();
+// $('player').empty();
+// $("#34").toggleClass("player");
+// $(this).append($(player).html());
+
+// player = $(player).contents().appendTo(this);
+// console.log(this);
 
 
 
-
-
-})
