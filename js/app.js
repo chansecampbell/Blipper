c380b4fd98ec27911a25a7d2792b705bdfcb7c23
chansecampbell <chansecampbell@gmail.com>
2016-03-25 16:09:15 +0000

    Completely lost. Got lots of code planned out but havent got very far.

--- js/app.js
+++ js/app.js
@@ -13,30 +13,78 @@ $(function(){
  game.obstacle = $(".obstacle").attr("class");
  game.moveCounter = 0;
  game.truthy = true;
+ game.currentPosition = 9;
 
  game.playerMove = function playerMove(){
   // Click event is set up to listen on all li's on the page.
   $("li").on("click", function(){
   // If a li is clicked that contains the class of the computer
-  if ($(this).attr("class") === game.computer){
-    alert("You can't move here.");
-  } else if ($(this).attr("class") === game.obstacle) {
-    alert("You can't move here.");
-  } else {
+  // if ($(this).attr("class") === game.computer){
+  //   alert("You can't move here.");
+  // } else if ($(this).attr("class") === game.obstacle) {
+  //   alert("You can't move here.");
+  // } else {
   // Else add the class 'player' to that spot and remove it from the currently existing one
-   if (this.id == (this.id - 1) || this.id == (this.id + 1) || this.id == (this.id - 6) || this.id == (this.id + 6) )
-   {
-    $(this).addClass('player').siblings('li').removeClass('player');
-   }
-   console.log(this.id);
+   // if (this.id == 3 || this.id == 8 || this.id == 10 || this.id == 15)
+   if (this.id == game.currentPosition + 1 || this.id == game.currentPosition - 1 || this.id == game.currentPosition + 6 || this.id == game.currentPosition - 6)
+   { $(this).addClass('player').siblings('li').removeClass('player');
+   
+    game.currentPosition = this.id;
+    console.log(this.id);
+  } 
+  
+  })
+
+
+  var borders = {
+    top : [0,1,2,3,4,5],
+    bottom : [30,31,32,33,34,35],
+    right : [5,11,17,23,29,35],
+    left : [0,6,12,18,24,30]
+  }
+
+  // $('body').on("keyup", function(event) {
+  //   event.preventDefault();
+  //   console.log(event);
+  //   if (e.which === 97) {
+  //     $(this).addClass('player').siblings('li').removeClass('player');
+  //     // check position isnt in borders.left
+  //     currentPosition-=1;
+
+      // function to check curent position and add player class to the right li
+
+    // }
+    //do for each keypress
+
+  }
+
+  // up 119
+  // down 115
+  // left 97
+  // right 100
+
+  // When you click right, new position is current position +1
+  // When you click up, new position is current position - grid width
+  // When you click left, new position is current position -1
+  // When you click down, new position is current position + grid width
+
+  // BUT 
+
+
+
+    
+  
+  
+
+   
   // This changes the computers move every time the player clicks and moves
    // $("div").removeClass().addClass("arrow-down")
   // Tracking moves made
    game.moveCounter++;
    // game.computerMove();
- }
- })
- }
+ // }
+
+ 
 
 // Function rotates the enemy
  // game.computerMove = function computerMove(){
