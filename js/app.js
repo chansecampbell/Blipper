c9f0cb98661092868f3cf0b0fa5e53b44c16a21b
chansecampbell <chansecampbell@gmail.com>
2016-03-27 20:23:38 +0100

    Have been going back and forth over code today trying to make my game work so the player can take the computer out but no luck all day. Got it working so saving and stopping..

--- js/app.js
+++ js/app.js
@@ -11,47 +11,66 @@ $(function(){
 
   var width = 6;
 
-  game.position = function position(i){
-   (width*width)-(i);
-  }
-
   game.gameBoard = function gameBoard(){
    $("body").append("<ul class='grid'></ul>");
    for (var i=0; i < (width*width); i++){
      $(".grid").append("<li id="+i+" class='empty'></li>");
-     $("#35").addClass("player").removeClass('empty');
-     $("#2").addClass("computer1-down").removeClass('empty');
-     $("#7").addClass("computer2-right").removeClass('empty');
-     $("#10").addClass("computer3-left").removeClass('empty');
-     $("#18").addClass("computer4-down").removeClass('empty');
-     $("#20").addClass("computer5-down").removeClass('empty');
-     $("#31").addClass("computer6-right").removeClass('empty');
-     $("#23").addClass("computer7-left").removeClass('empty');
-     $("#1").addClass("obstacle").removeClass('empty');
-     $("#4").addClass("obstacle").removeClass('empty');
-     $("#12").addClass("obstacle").removeClass('empty');
-     $("#14").addClass("obstacle").removeClass('empty');
-     $("#15").addClass("obstacle").removeClass('empty');
-     $("#17").addClass("obstacle").removeClass('empty');
-     $("#25").addClass("obstacle").removeClass('empty');
-     $("#28").addClass("obstacle").removeClass('empty');
-   }
-  }
+     
+     this.currentPosition = (width*width)-(1);
+     this.moveCounter = 0;
+     this.truthy = true;
+     game.levels[0];
 
-  game.start = function(){
-    this.obstacle = $("obstacle");
-    this.player = $("player");
-    this.moveCounter = 0;
-    this.truthy = true;
-    this.currentPosition = 35;
-    this.$squares = $('li');
-    this.borders = { // else var borders =
-      top : [0,1,2,3,4,5],
-      bottom : [30,31,32,33,34,35],
-      right : [5,11,17,23,29,35],
-      left : [0,6,12,18,24,30] };
-    
-  }
+     game.levels[0].player = $("#35").addClass("player").removeClass('empty');
+     game.levels[0].computers.one =  $("#2").addClass("computer1-down").removeClass('empty');
+     game.levels[0].computers.two = $("#7").addClass("computer2-right").removeClass('empty');
+     game.levels[0].computers.three = $("#10").addClass("computer3-left").removeClass('empty');
+     game.levels[0].computers.four = $("#18").addClass("computer4-down").removeClass('empty');
+     game.levels[0].computers.five = $("#20").addClass("computer5-down").removeClass('empty');
+     game.levels[0].computers.six = $("#31").addClass("computer6-right").removeClass('empty');
+     game.levels[0].computers.seven = $("#23").addClass("computer7-left").removeClass('empty');
+     game.levels[0].obstacles[0] = $("#1").addClass("obstacle").removeClass('empty');
+     game.levels[0].obstacles[1] = $("#4").addClass("obstacle").removeClass('empty');
+     game.levels[0].obstacles[2] = $("#12").addClass("obstacle").removeClass('empty');
+     game.levels[0].obstacles[3] = $("#14").addClass("obstacle").removeClass('empty');
+     game.levels[0].obstacles[4] =  $("#15").addClass("obstacle").removeClass('empty');
+     game.levels[0].obstacles[5] = $("#17").addClass("obstacle").removeClass('empty');
+     game.levels[0].obstacles[6] = $("#25").addClass("obstacle").removeClass('empty');
+     game.levels[0].obstacles[7] = $("#28").addClass("obstacle").removeClass('empty');
+     
+   }
+ }
+
+ game.levels = [
+ {
+  obstacles: [1,4,12,14,15,17,25,28],
+  computers: {
+    one: 2,
+    two: 7,
+    three: 10,
+    four: 18,
+    five: 20,
+    six: 31,
+    seven: 23
+  },
+  player: 35
+},
+]
+
+  // game.start = function(){
+  //   this.obstacle = $("obstacle");
+  //   this.player = $("player");
+  //   this.moveCounter = 0;
+  //   this.truthy = true;
+  //   this.currentPosition = (width*width)-(1);
+  //   this.$squares = $('li');
+  //   this.borders = { // else var borders =
+  //     top : [0,1,2,3,4,5],
+  //     bottom : [30,31,32,33,34,35],
+  //     right : [5,11,17,23,29,35],
+  //     left : [0,6,12,18,24,30] };
+
+  // }
 
 
 // This checks the value of current positions number against an array. Isnt yet integrated
@@ -66,7 +85,7 @@ $(function(){
 //      console.log("out of bounds");
 //    })
 //  }
-    
+
 // }
 // game.checkBorders(30);
 // When you click right, new position is current position +1
@@ -82,149 +101,152 @@ game.playerMove = function playerMove(){
     /// UP
     if (e.which === 38) {
       $($("li")[game.currentPosition]).removeClass("player");
-      game.currentPosition-=6;
+      game.currentPosition-=width;
 
-        if ($($("li")[game.currentPosition]).hasClass("obstacle")) {
-          console.log("You can't walk here!");
-        game.currentPosition+=6;
+      if ($($("li")[game.currentPosition]).hasClass("obstacle")) {
+        console.log("You can't walk here!");
+        game.currentPosition+=width;
         $($("li")[game.currentPosition]).addClass("player").removeClass("empty");
-        } 
-        else {
-      $($("li")[game.currentPosition]).addClass("player").css("animation-name", "slideInUp").removeClass('empty');
+      } 
+      else {
+        $($("li")[game.currentPosition]).addClass("player").css("animation-name", "slideInUp").removeClass('empty');
+        $('#slide').get(0).play();
+        game.moveCounter++;
+        game.computerMove();
+      }
+    /// RIGHT
+  } else if (e.which === 39) {
+    $($("li")[game.currentPosition]).removeClass("player");
+    game.currentPosition+=1;
+
+    if ($($("li")[game.currentPosition]).hasClass("obstacle")) {
+      console.log("You can't walk here!");
+      game.currentPosition-=1;
+      $($("li")[game.currentPosition]).addClass("player").removeClass("empty");
+    } 
+    else {
+      $($("li")[game.currentPosition]).addClass("player").css("animation-name", "slideInLeft").removeClass('empty');
       $('#slide').get(0).play();
       game.moveCounter++;
       game.computerMove();
-        }
-    /// RIGHT
-    } else if (e.which === 39) {
-    $($("li")[game.currentPosition]).removeClass("player");
-    game.currentPosition+=1;
-     
-      if ($($("li")[game.currentPosition]).hasClass("obstacle")) {
-        console.log("You can't walk here!");
-      game.currentPosition-=1;
-      $($("li")[game.currentPosition]).addClass("player").removeClass("empty");
-      } 
-      else {
-    $($("li")[game.currentPosition]).addClass("player").css("animation-name", "slideInLeft").removeClass('empty');
-    $('#slide').get(0).play();
-    game.moveCounter++;
-    game.computerMove();
-      }
+    }
     /// DOWN
-    } else if (e.which === 40) {
-      $($("li")[game.currentPosition]).removeClass("player");
-          game.currentPosition+=6;
-           
-            if ($($("li")[game.currentPosition]).hasClass("obstacle")) {
-              console.log("You can't walk here!");
-            game.currentPosition-=6;
-            $($("li")[game.currentPosition]).addClass("player").removeClass("empty");
-            } 
-            else {
-          $($("li")[game.currentPosition]).addClass("player").css("animation-name", "slideInDown").removeClass('empty');
-          $('#slide').get(0).play();
-          game.moveCounter++;
-          game.computerMove();
-            }
+  } else if (e.which === 40) {
+    $($("li")[game.currentPosition]).removeClass("player");
+    game.currentPosition+=width;
+
+    if ($($("li")[game.currentPosition]).hasClass("obstacle")) {
+      console.log("You can't walk here!");
+      game.currentPosition-=width;
+      $($("li")[game.currentPosition]).addClass("player").removeClass("empty");
+    } 
+    else {
+      $($("li")[game.currentPosition]).addClass("player").css("animation-name", "slideInDown").removeClass('empty');
+      $('#slide').get(0).play();
+      game.moveCounter++;
+      game.computerMove();
+    }
     //// LEFT
-    } else if (e.which === 37) {
-     $($("li")[game.currentPosition]).removeClass("player");
-     game.currentPosition-=1;
-      
-       if ($($("li")[game.currentPosition]).hasClass("obstacle")) {
-         console.log("You can't walk here!");
-       game.currentPosition+=1;
-       $($("li")[game.currentPosition]).addClass("player").removeClass("empty");
-       } 
-       else {
+  } else if (e.which === 37) {
+   $($("li")[game.currentPosition]).removeClass("player");
+   game.currentPosition-=1;
+
+   if ($($("li")[game.currentPosition]).hasClass("obstacle")) {
+     console.log("You can't walk here!");
+     game.currentPosition+=1;
+     $($("li")[game.currentPosition]).addClass("player").removeClass("empty");
+   } 
+   else {
      $($("li")[game.currentPosition]).addClass("player").css("animation-name", "slideInRight").removeClass('empty');
      $('#slide').get(0).play();
      game.moveCounter++;
      game.computerMove();
-       }
+   }
 
-    }
-    game.detection();
-    game.checkForWin();
-    $("#score").html(game.moveCounter);
-  })
+ }
+ game.detection();
+ game.checkForWin();
+ $("#score").html(game.moveCounter);
+})
 
-  }
+}
 
 // Function rotates the enemy
- game.computerMove = function computerMove(){
+game.computerMove = function computerMove(){
   if (game.truthy === true){
-  $("#2").addClass("computer1-right").removeClass('computer1-down');
-  $("#7").addClass("computer2-down").removeClass('computer2-right');
-  $("#10").addClass("computer3-down").removeClass('computer3-left');
-  $("#18").addClass("computer4-right").removeClass('computer4-down');
-  $("#20").addClass("computer5-right").removeClass('computer5-down');
-  $("#31").addClass("computer6-left").removeClass('computer6-right');
-  $("#23").addClass("computer7-down").removeClass('computer7-left');
+    $("#2").addClass("computer1-right").removeClass('computer1-down');
+    $("#7").addClass("computer2-down").removeClass('computer2-right');
+    $("#10").addClass("computer3-down").removeClass('computer3-left');
+    $("#18").addClass("computer4-right").removeClass('computer4-down');
+    $("#20").addClass("computer5-right").removeClass('computer5-down');
+    $("#31").addClass("computer6-left").removeClass('computer6-right');
+    $("#23").addClass("computer7-down").removeClass('computer7-left');
     game.truthy = false;
   } else {
-  $("#2").addClass("computer1-down").removeClass('computer1-right');
-  $("#7").addClass("computer2-right").removeClass('computer2-down');
-  $("#10").addClass("computer3-left").removeClass('computer3-down');
-  $("#18").addClass("computer4-down").removeClass('computer4-right');
-  $("#20").addClass("computer5-down").removeClass('computer5-right');
-  $("#31").addClass("computer6-right").removeClass('computer6-left');
-  $("#23").addClass("computer7-left").removeClass('computer7-down');
+    $("#2").addClass("computer1-down").removeClass('computer1-right');
+    $("#7").addClass("computer2-right").removeClass('computer2-down');
+    $("#10").addClass("computer3-left").removeClass('computer3-down');
+    $("#18").addClass("computer4-down").removeClass('computer4-right');
+    $("#20").addClass("computer5-down").removeClass('computer5-right');
+    $("#31").addClass("computer6-right").removeClass('computer6-left');
+    $("#23").addClass("computer7-left").removeClass('computer7-down');
     game.truthy = true;
   }
 }
+//// I need a way to say that if player has an ID of 2, 7, 10, 18, 20, 31 or 23 then this value needs to be removed.
 
 // Check for a win
- game.checkForWin = function checkForWin(){
+game.checkForWin = function checkForWin(){
   if ($("#0").hasClass("player")) {
     $("h3").append("Congratulations, you beat the level!");
     $($("li").addClass("won").css("animation-name", "pulse"));
     $('#end').get(0).play();
     alert("You've completed the level!");
     // Need to add in a feature to end the game and reset the level here.
- }
+  }
 }
 
 
   // Win conditions
-   game.detection = function detection(i){
+  game.detection = function detection(i){
     for (i = 0; i < 8; i++) {
-   var compRight = $('.computer'+i+'-right').attr('id');
-   compRight = parseInt(compRight);
-   var compLeft = $('.computer'+i+'-left').attr('id');
-   compLeft = parseInt(compLeft);
-   var compUp = $('.computer'+i+'-up').attr('id');
-   compUp = parseInt(compUp);
-   var compDown = $('.computer'+i+'-down').attr('id');
-   compDown = parseInt(compDown);
-   var playerLocation = $('.player').attr('id');
-   playerLocation = parseInt(playerLocation);
-   console.log(compDown);
-   console.log(playerLocation);
+     var compRight = $('.computer'+i+'-right').attr('id');
+     compRight = parseInt(compRight);
+     var compLeft = $('.computer'+i+'-left').attr('id');
+     compLeft = parseInt(compLeft);
+     var compUp = $('.computer'+i+'-up').attr('id');
+     compUp = parseInt(compUp);
+     var compDown = $('.computer'+i+'-down').attr('id');
+     compDown = parseInt(compDown);
+     var playerLocation = $('.player').attr('id');
+     playerLocation = parseInt(playerLocation);
+   // console.log(compDown);
+   // console.log(playerLocation);
 
-     if (playerLocation  == (compRight + 1)) {
+   if (playerLocation  == (compRight + 1)) {
     $($("li").addClass("caught").css("animation-name", "pulse"));
     $('#busted').get(0).play();
     alert("You've been caught!");
-     } else if (playerLocation == (compLeft - 1)) {
-      $($("li").addClass("caught").css("animation-name", "pulse"));
-      $('#busted').get(0).play();
-      alert("You've been caught!");
-     } else if (playerLocation == (compUp - 6)) {
-      $($("li").addClass("caught").css("animation-name", "pulse"));
-      $('#busted').get(0).play();
-      alert("You've been caught!");
-     } else if (playerLocation == (compDown + 6)) {
-      $($("li").addClass("caught").css("animation-name", "pulse"));
-      $('#busted').get(0).play();
-      alert("You've been caught!");
-     } 
-   }
-   }
- 
- game.gameBoard();
- game.start();
+  } else if (playerLocation == (compLeft - 1)) {
+    $($("li").addClass("caught").css("animation-name", "pulse"));
+    $('#busted').get(0).play();
+    alert("You've been caught!");
+  } else if (playerLocation == (compUp - 6)) {
+    $($("li").addClass("caught").css("animation-name", "pulse"));
+    $('#busted').get(0).play();
+    alert("You've been caught!");
+  } else if (playerLocation == (compDown + 6)) {
+    $($("li").addClass("caught").css("animation-name", "pulse"));
+    $('#busted').get(0).play();
+    alert("You've been caught!");
+  } else if (playerLocation === compRight) {
+    $($("li")[game.currentPosition]).addClass("player").css("animation-name", "slideInRight").removeClass('.computer'+i+'-right');
+  }
+}
+}
+
+game.gameBoard();
+ // game.start();
  game.playerMove();
 })
 
--- newgrid.js
+++ newgrid.js
@@ -1,22 +1,63 @@
 $(function(){
 
+/// Planning for changing the game into a multi-level game.
 
+/// then your game.gameBoard just plugs in these values. You would feed in a value of level number depending what level you’re on, and level 1 would use game.levels[0], level 2 would used game.levels[1] etc etc
+// So for each level, you would generate the game board using the object for that level
+// Lvl1
+game.levels = [
+  {
+    obstacles: [1,4,12,14,15,17,25,28],
+    computers: {
+      1: 2,
+      2: 7,
+      3: 10,
+      4: 18,
+      5: 20,
+      6: 31,
+      7: 23
+    },
+    player: 35
+  },
 
-var width = 6;
-var start = (width*width)-(width);
-var path  = [start];
+  {
+     obstacles: [1,4,12,14,15,17,25,28],
+     computers: {
+       1: 2,
+       2: 7,
+       3: 10,
+       4: 18,
+       5: 20,
+       6: 31,
+       7: 23
+     },
+     player: 35
+   }, 
 
-var gameBoard = function gameBoard(){
- $("body").append("<ul class='grid'></ul>");
- for (var i=0; i < (width*width); i++){
-   $(".grid").append("<li id="+i+" class='empty'></li>")
- }
-}
-
-$(".grid").addClass("player").removeClass('empty');
-$("#2").addClass("computer-down").removeClass('empty');
-$("#31").addClass("computer-left").removeClass('empty');
-
-gameBoard();
+]
 
 })
+
+
+game.gameBoard = function gameBoard(){
+ $("body").append("<ul class='grid'></ul>");
+ for (var i=0; i < (width*width); i++){
+   $(".grid").append("<li id="+i+" class='empty'></li>");
+   $("#35").addClass("player").removeClass('empty');
+   $("#2").addClass("computer1-down").removeClass('empty');
+   $("#7").addClass("computer2-right").removeClass('empty');
+   $("#10").addClass("computer3-left").removeClass('empty');
+   $("#18").addClass("computer4-down").removeClass('empty');
+   $("#20").addClass("computer5-down").removeClass('empty');
+   $("#31").addClass("computer6-right").removeClass('empty');
+   $("#23").addClass("computer7-left").removeClass('empty');
+   $("#1").addClass("obstacle").removeClass('empty');
+   $("#4").addClass("obstacle").removeClass('empty');
+   $("#12").addClass("obstacle").removeClass('empty');
+   $("#14").addClass("obstacle").removeClass('empty');
+   $("#15").addClass("obstacle").removeClass('empty');
+   $("#17").addClass("obstacle").removeClass('empty');
+   $("#25").addClass("obstacle").removeClass('empty');
+   $("#28").addClass("obstacle").removeClass('empty');
+ }
+}
